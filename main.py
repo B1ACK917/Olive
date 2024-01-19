3f3473e7bab3f052286aef17da11d530e2593828
B1ACK917 <397615103@qq.com>
2024-01-19 15:05:18 +0800

    add: dense resnet & unet benchmark

--- main.py
+++ main.py
@@ -1,58 +1,10 @@
-import pickle
-
-import torch
-import torch.utils.benchmark as benchmark_utils
-from tqdm import tqdm
-
-from utils.bench import gen_sparse_input
+from utils.bench import bench_base, bench_resnet50, bench_unet
 
 
 def main():
-    tasks = [
-        ("matmul", "torch.sparse.mm(x, y)", "torch.sparse.mm(sx, y)"),
-        ("matmul", "torch.mm(x,y)", "torch.mm(dx,y)"),
-
-        ("sum", "torch.sparse.sum(x,dim=0)", "torch.sparse.sum(sx,dim=0)"),
-        ("sum", "torch.sum(x,dim=0)", "torch.sum(dx,dim=0)"),
-
-        ("softmax", "torch.sparse.softmax(x,dim=0)", "torch.sparse.softmax(sx,dim=0)"),
-        ("softmax", "torch.softmax(x,dim=0)", "torch.softmax(dx,dim=0)"),
-    ]
-
-    serialized_results, timers = [], []
-    repeats = 2
-    for label, sub_label, stmt in tasks:
-        for density in [i / 100.0 for i in range(5, 100, 5)]:
-            for size in [(8, 8), (32, 32), (64, 64), (128, 128)]:
-                sx, dx = gen_sparse_input(size=size, density=density, dtype=torch.float32)
-                y = torch.rand(size, dtype=torch.float32)
-                timers.append(
-                    benchmark_utils.Timer(
-                        stmt=stmt,
-                        globals={
-                            "torch": torch,
-                            "sx": sx,
-                            "dx": dx,
-                            "y": y,
-                            "zero": torch.zeros(()),
-                        },
-                        label=label,
-                        sub_label=sub_label,
-                        env=str(f"density: {density}"),
-                        description=f"size: {size}"
-                    )
-                )
-
-    for timer in tqdm(timers * repeats, postfix="Benchmarking"):
-        serialized_results.append(pickle.dumps(
-            timer.blocked_autorange(min_run_time=0.05)
-        ))
-
-    comparison = benchmark_utils.Compare([
-        pickle.loads(i) for i in serialized_results
-    ])
-
-    comparison.print()
+    # bench_base()
+    # bench_resnet50()
+    bench_unet()
 
 
 if __name__ == "__main__":
--- utils/bench.py
+++ utils/bench.py
@@ -1,21 +1,140 @@
+import pickle
+
 import torch
+import torch.utils.benchmark as benchmark_utils
+from tqdm import tqdm
+from model.resnet import ResNet50
+from model.unet import UNet
 
 
-def generate_coo_data(size, sparse_dim, nnz, dtype):
-    if dtype is None:
-        dtype = 'float32'
-
-    indices = torch.rand(sparse_dim, nnz)
-    indices.mul_(torch.tensor(size[:sparse_dim]).unsqueeze(1).to(indices))
-    indices = indices.to(torch.long)
-    values = torch.rand([nnz, ], dtype=dtype)
-    return indices, values
-
-
-def gen_sparse_input(size, density, dtype):
-    sparse_dim = len(size)
+def generate_sparse_matrix_2d(size, density, dtype):
     nnz = int(size[0] * size[1] * density)
-    indices, values = generate_coo_data(size, sparse_dim, nnz, dtype)
-    sparse = torch.sparse_coo_tensor(indices, values, size, dtype=dtype)
-    dense = sparse.to_dense()
-    return sparse, dense
+    indices = torch.randint(0, size[0], (nnz,))
+    indices = torch.cat([indices.unsqueeze(0), torch.randint(0, size[1], (nnz,)).unsqueeze(0)], dim=0)
+    values = torch.randn(nnz)
+    sparse_matrix = torch.sparse_coo_tensor(indices, values, size, dtype=dtype)
+    return sparse_matrix
+
+
+def generate_sparse_tensor_3d(size, density, dtype):
+    nnz = int(size[0] * size[1] * size[2] * density)
+    indices = torch.randint(0, size[0], (nnz,))
+    indices = torch.cat([indices.unsqueeze(0), torch.randint(0, size[1], (nnz,)).unsqueeze(0),
+                         torch.randint(0, size[2], (nnz,)).unsqueeze(0)], dim=0)
+    values = torch.randn(nnz)
+    sparse_tensor = torch.sparse_coo_tensor(indices, values, size, dtype=dtype)
+    return sparse_tensor, indices, values
+
+
+def bench(timers, repeats):
+    serialized_results = []
+    for timer in tqdm(timers * repeats, postfix="Benchmarking"):
+        serialized_results.append(pickle.dumps(
+            timer.blocked_autorange(min_run_time=0.05)
+        ))
+
+    comparison = benchmark_utils.Compare([
+        pickle.loads(i) for i in serialized_results
+    ])
+
+    comparison.print()
+
+
+def bench_base():
+    tasks = [
+        ("matmul", "torch.sparse.mm(x, y)", "torch.sparse.mm(sx, y)"),
+        ("matmul", "torch.mm(x,y)", "torch.mm(dx,y)"),
+        #
+        # ("sum", "torch.sparse.sum(x,dim=0)", "torch.sparse.sum(sx,dim=0)"),
+        # ("sum", "torch.sum(x,dim=0)", "torch.sum(dx,dim=0)"),
+        #
+        # ("softmax", "torch.sparse.softmax(x,dim=0)", "torch.sparse.softmax(sx,dim=0)"),
+        # ("softmax", "torch.softmax(x,dim=0)", "torch.softmax(dx,dim=0)"),
+    ]
+
+    timers = []
+    repeats = 2
+    for label, sub_label, stmt in tasks:
+        for density in [i / 100.0 for i in range(5, 100, 5)]:
+            for size in [(8, 8), (32, 32), (64, 64), (128, 128)]:
+                sx = generate_sparse_matrix_2d(size=size, density=density, dtype=torch.float32)
+                dx = sx.to_dense()
+                y = torch.rand(size, dtype=torch.float32)
+                timers.append(
+                    benchmark_utils.Timer(
+                        stmt=stmt,
+                        globals={
+                            "torch": torch,
+                            "sx": sx,
+                            "dx": dx,
+                            "y": y
+                        },
+                        label=label,
+                        sub_label=sub_label,
+                        env=str(f"density: {density}"),
+                        description=f"size: {size}"
+                    )
+                )
+    bench(timers, repeats)
+
+
+def bench_resnet50():
+    tasks = [
+        ("ResNet 50", "Dense Implementation", "model(x)"),
+    ]
+
+    timers = []
+    repeats = 2
+    model = ResNet50(2)
+    for label, sub_label, stmt in tasks:
+        for density in [i / 100.0 for i in range(5, 100, 5)]:
+            for size in [(3, 64, 64), (3, 128, 128), (3, 256, 256)]:
+                sx, *_ = generate_sparse_tensor_3d(size=size, density=density, dtype=torch.float32)
+                dx = sx.to_dense().unsqueeze(dim=0)
+                timers.append(
+                    benchmark_utils.Timer(
+                        stmt=stmt,
+                        globals={
+                            "torch": torch,
+                            "model": model,
+                            "x": dx,
+                        },
+                        label=label,
+                        sub_label=sub_label,
+                        env=str(f"density: {density}"),
+                        description=f"size: {size}"
+                    )
+                )
+    bench(timers, repeats)
+
+
+def bench_unet():
+    tasks = [
+        ("UNet", "Dense Implementation", "unet(x)"),
+        # ("UNet", "Sparse Implementation", "sparse_unet(x)"),
+    ]
+
+    timers = []
+    repeats = 2
+    unet = UNet(3, 2)
+    for label, sub_label, stmt in tasks:
+        for density in [i / 100.0 for i in range(5, 100, 5)]:
+            for size in [(3, 64, 64), (3, 128, 128), (3, 256, 256)]:
+                sx, indices, values = generate_sparse_tensor_3d(size=size, density=density, dtype=torch.float32)
+                dx = sx.to_dense().unsqueeze(dim=0)
+                coo = [indices, values]
+                timers.append(
+                    benchmark_utils.Timer(
+                        stmt=stmt,
+                        globals={
+                            "torch": torch,
+                            "unet": unet,
+                            "x": dx,
+                        },
+                        label=label,
+                        sub_label=sub_label,
+                        env=str(f"density: {density}"),
+                        description=f"size: {size}"
+                    )
+                )
+    bench(timers, repeats)
